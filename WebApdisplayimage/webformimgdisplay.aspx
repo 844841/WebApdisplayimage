3b9754970e352e1bbee519af0af874db1f40bcc3
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 16:29:01 +0530

    Initial commit

---
+++ WebApdisplayimage/webformimgdisplay.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" MasterpageFile="~/Site.Master" CodeBehind="webformimgdisplay.aspx.cs" Inherits="WebApdisplayimage.webformimgdisplay" %>
+
+<asp:Content ID="id123" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron">
+        <asp:FileUpload ID="FileUpload1" runat="server"/>
+        <br />
+        <br />
+      
+        <asp:Button ID="Button1" runat="server" Text="Upload" OnClick="Buttonsup" />
+        <br />
+        <br />
+        <asp:DataList ID="DataList1" runat="server" RepeatColumns="2" >
+            <ItemTemplate>
+                <asp:Image ID="Image1"
+                 ImageUrl='<%# Eval("Name","~/Resources/{0}") %>'
+                    Width="350px" Height="350px" runat="server" />
+                <br />
+                <%# Eval("Name")%>
+               
+                </ItemTemplate>              
+
+        </asp:DataList>    </div>
+    </asp:Content>
---
+++ WebApdisplayimage/webformimgdisplay.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApdisplayimage
+{
+    public partial class webformimgdisplay : System.Web.UI.Page
+    {
+        void Page_PreRender()
+        {
+            string uploadfolder = MapPath("~/Resources/");
+            DirectoryInfo dir = new DirectoryInfo(uploadfolder);
+            DataList1.DataSource = dir.GetFiles();
+            DataList1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool CheckFileType(string fileName)
+        {
+            string ext = Path.GetExtension(fileName);
+            switch(ext.ToLower())
+            {
+                case ".gif":
+                    return true;
+                case ".png":
+                    return true;
+                case ".jpg":
+                    return true;
+                case ".jpeg":
+                    return true;
+                default:
+                    return false;
+
+
+            }
+
+            
+        }
+
+        protected void Buttonsup(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFiles)
+            {
+                if (CheckFileType(FileUpload1.FileName))
+                {
+                    FileUpload1.SaveAs(MapPath("~/Resources/" + FileUpload1.FileName));
+                }
+            }
+        }
+    }
+}
